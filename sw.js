dbf2cd76453d2740de1ebd7dd9908cf771e885cb
Lubos Belak <belak@creanet.sk>
2016-09-30 13:49:44 +0200

    Adding a ServiceWorker

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'SW1';
+const filesToCache = [
+    'index.html',
+    'cursor.svg'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(cache => cache.addAll(filesToCache))
+            .catch(err => console.error(err));
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches
+            .keys()
+            .then(cacheNames => Promise.all(
+                cacheNames
+                    .filter(cacheName => cacheName !== CACHE_NAME)
+                    .map(cacheName => cache.delete(cacheName));
+            ))
+            .catch(err => console.error(err));
+    );
+});
