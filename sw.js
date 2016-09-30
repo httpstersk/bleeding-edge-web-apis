25166727e7b67cecfd0658daf88e5d8ba910c8a6
Lubos Belak <belak@creanet.sk>
2016-09-30 14:07:10 +0200

    Rewriting ServiceWorker to ES5 syntax

--- sw.js
+++ sw.js
@@ -1,25 +1,31 @@
 const CACHE_NAME = 'SW1';
-const filesToCache = [
+const FILES_TO_CACHE = [
     'index.html',
     'cursor.svg'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(cache => cache.addAll(filesToCache));
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(FILES_TO_CACHE);
+            })
+            .then(function() {
+                return self.skipWaiting();
+            })
     );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', function(event) {
     event.waitUntil(
-        caches
-            .keys()
-            .then(cacheNames => Promise.all(
-                cacheNames
-                    .filter(cacheName => cacheName !== CACHE_NAME)
-                    .map(cacheName => cache.delete(cacheName));
-            ));
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName !== CACHE_NAME;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
     );
 });
